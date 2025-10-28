fd8eb4a0e25a9e253d6591b0ad2fa1fb95174911
Parsa <Parsa@DESKTOP>
2025-10-28 23:28:26 +0330

    Add project files.

---
+++ 0003/Program.cs
@@ -0,0 +1,55 @@
+﻿//Tamrin03.MohammadparsaAkkaf
+
+Console.WriteLine("Enter the number");
+
+do
+{
+    String input = Console.ReadLine();
+
+    int n = int.Parse(input);
+
+    for (int p = 2; p < n - 1; p++)
+    {
+        int m = n - p;
+
+        bool x = true;
+        bool y = true;
+
+
+        for (int k = p - 1; k > 1; k--)
+        {
+            if (p % k == 0)
+            {
+                x = false;
+                break;
+            }
+        }
+
+
+        for (int k = m - 1; k > 1; k--)
+        {
+            if (m % k == 0)
+            {
+                y = false;
+                break;
+            }
+        }
+
+        if (x == false)
+        {
+            if (x == false)
+            {
+
+                Console.WriteLine(p + "," + m);
+
+            }
+        }
+    }
+
+
+
+
+    Console.WriteLine("........................");
+    Console.WriteLine("Now enter another number");
+}
+while (true);
